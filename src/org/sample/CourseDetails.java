4ac35ebc99dc32f67d3c5d325baac76d53677ade
Sabasiva06 <sabasvia06@gmail.com>
2021-06-12 18:17:30 +0530

    changes done on baseclass

---
+++ src/org/sample/CourseDetails.java
@@ -0,0 +1,36 @@
+package org.sample;
+
+public class CourseDetails {
+
+	public static void main(String[] args) {
+		byte b = 10;
+		System.out.println("Byte value :" + b);
+
+		short s = 23456;
+		System.out.println("Short value :" + s);
+
+		int i = 4567898;// we can store maxx of 9 digit no
+		System.out.println("Int value :" + i);
+
+		// in long we can strore more than 10 digit number
+		long l = 9087322345l;
+		System.out.println("long value :" + l);
+
+		float f = 356478.986637f;
+		System.out.println("float value :" + f);
+
+		double d = 4569.8776745678;
+		System.out.println("Double value :" + d);
+
+		boolean w = true;
+		System.out.println("Boolean value :" + w);
+
+		char c = 'm';
+		System.out.println("Character value :" + c);
+
+		String ss = "Java programm";
+		System.out.println("String value :" + ss);
+
+	}
+
+}
---
+++ src/org/sample/Test.java
@@ -0,0 +1,24 @@
+package org.sample;
+
+import java.util.Scanner;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		
+		System.out.println("Enter an Year :: ");
+		Scanner sc = new Scanner(System.in);
+		int year = sc.nextInt();
+
+		if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
+			System.out.println("Specified year is a leap year");
+		else
+			System.out.println("Specified year is not a leap year");
+	}
+}
